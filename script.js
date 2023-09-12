19af7e54d51ceb6920597a30ebb6a9c866cd7236
nicknufayl <nikndaniel@gmail.com>
2023-09-12 22:05:04 +0800

    General UI and Style add
    style.css and buttons are now added

---
+++ script.js
@@ -0,0 +1,20 @@
+let a = prompt("Please enter the first number: ");
+let b = prompt("Please enter the second number: ");
+let num1 = parseInt(a);
+let num2 = parseInt(b);
+
+add = (num1, num2) => {
+
+}
+
+subtract = (num1, num2) => {
+
+}
+
+add = (num1, num2) => {
+
+}
+
+add = (num1, num2) => {
+
+}
